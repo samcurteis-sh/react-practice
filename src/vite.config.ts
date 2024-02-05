67f5fe0b7f6bc6ec53ba3d56eb6f3d4a9c1167ec
samcurteis-sh <146755918+samcurteis-sh@users.noreply.github.com>
2024-02-05 16:51:47 +0000

    set up a basic config for multi-page vite application

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,6 +3,7 @@
 
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,17 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     css: true,
+  },
+  root: 'src',
+  build: {
+    outDir: path.join(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+        input: {
+          index: path.resolve(__dirname, 'index.tsx'),
+          discount: path.resolve(__dirname, 'discount.tsx'),
+        },
+    },
   }
+
 })
